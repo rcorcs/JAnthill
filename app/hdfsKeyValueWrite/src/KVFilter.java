1bb460d4fba0bbc653de378e4f76d64dfb0ff3c5
Rodrigo Caetano Rocha <rcor.cs@gmail.com>
2013-07-30 02:21:23 -0300

    beta version

--- app/hdfsKeyValueWrite/src/KVFilter.java
+++ app/hdfsKeyValueWrite/src/KVFilter.java
@@ -1,6 +1,6 @@
 /**
  * @author Rodrigo Caetano O. ROCHA
- * @date 24 July 2013
+ * @date 30 July 2013
  */
 
 import java.io.IOException;
@@ -19,6 +19,9 @@ public class KVFilter extends Filter<String, SimpleEntry<String,String> >{
 	public void process(String data){
 		try{
 			String []strs = data.split("\\W");//non-word characters split
+
+			//for each word emits a pair <word, 1> counting one more occurence of the word.
+			//this example is the map phase of the count word MapReduce common application
 			for(String word : strs){
 				if(word.length()>0){
 					SimpleEntry<String,String> pair = new SimpleEntry<String,String>(word, "1");
